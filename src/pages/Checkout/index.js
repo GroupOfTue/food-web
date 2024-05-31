7404703897bc73aca544d309579b62c8c73da804
Qthang <tqtbest@gmail.com>
2024-05-31 20:04:16 +0700

    fix order(checkout) with database

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -17,75 +17,37 @@ function Checkout() {
           <main className="col-md-9">
             <div className="card">
               <div className="card-body border-top">
-                <div className="row">
-                  <div className="col-md-6">
-                    <form class="form">
+                <form class="form">
+                  <div class="containerx">
+                    <div class="col-50">
+                      <BillingInfo /> {/* Call BillingInfo component */}{' '}
+                      <label for="fullname">
+                        <i class="fa fa-user">&nbsp;</i>Full Name
+                      </label>
+                      <input type="text" id="fullname" placeholder="Mr John" />
+                      <label for="email">
+                        <i class="fa fa-envelope">&nbsp;</i>Full Name
+                      </label>
+                      <input type="text" id="email" placeholder="John_admin@mail.com" />
+                      <label for="address">
+                        <i class="fa fa-address-card-o">&nbsp;</i>Address
+                      </label>
+                      <input type="text" id="address" placeholder="542 st sant serit" />
+                      <label for="city">
+                        <i class="fa fa-institution">&nbsp;</i>Phone number
+                      </label>
+                      <input type="text" id="city" placeholder="Your number" />
                       <div class="containerx">
                         <div class="col-50">
-                          <BillingInfo /> {/* Call BillingInfo component */}{' '}
-                          <label for="fullname">
-                            <i class="fa fa-user">&nbsp;</i>Full Name
-                          </label>
-                          <input type="text" id="fullname" placeholder="Mr John" />
-                          <label for="email">
-                            <i class="fa fa-envelope">&nbsp;</i>Full Name
-                          </label>
-                          <input type="text" id="email" placeholder="John_admin@mail.com" />
-                          <label for="address">
-                            <i class="fa fa-address-card-o">&nbsp;</i>Address
-                          </label>
-                          <input type="text" id="address" placeholder="542 st sant serit" />
-                          <label for="city">
-                            <i class="fa fa-institution">&nbsp;</i>City
-                          </label>
-                          <input type="text" id="city" placeholder="New York" />
-                          <div class="containerx">
-                            <div class="col-50">
-                              <label for="state">State</label>
-                              <input type="text" id="state" placeholder="NY" />
-                            </div>
-                            <div class="col-50">
-                              <label for="zip">Zip</label>
-                              <input type="text" id="zip" placeholder="10001" />
-                            </div>
-                          </div>
+                          <label for="note">Note</label>
+
+                          <textarea className={cx('text-area')} rows="7" cols="117" name="myTextarea"></textarea>
                         </div>
                       </div>
-                    </form>
+                    </div>
                   </div>
-                  <div className="col-md-6">
-                    <form class="form">
-                      <div class="containerx">
-                        <div class="col-50">
-                          <PaymentInfo /> {/* Call PaymentInfo component */}
-                          <label>Accepted Cards</label>
-                          <div class="icon-cards">
-                            <i class="fa fa-cc-visa">&nbsp;</i>
-                            <i class="fa fa-cc-amex">&nbsp;</i>
-                            <i class="fa fa-cc-mastercard">&nbsp;</i>
-                            <i class="fa fa-cc-discover">&nbsp;</i>
-                          </div>
-                          <label for="namecard">Name on Card</label>
-                          <input type="text" id="namecard" placeholder="John Morgant" />
-                          <label for="cardnumber">Credit card number</label>
-                          <input type="text" id="cardnumber" placeholder="1111-2584-2354-2541" />
-                          <label for="expmonth">Exp Month</label>
-                          <input type="text" id="expmonth" placeholder="September" />
-                          <div class="containerx">
-                            <div class="col-50">
-                              <label for="expyear">Exp Year</label>
-                              <input type="text" id="expyear" placeholder="2018" />
-                            </div>
-                            <div class="col-50">
-                              <label for="cvv">CVV</label>
-                              <input type="text" id="cvv" placeholder="352" />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
+                </form>
+
                 <a href="#" className={clsx('btn btn-primary float-md-right', cx('checkout-btn'))}>
                   Finish <i className="fa fa-chevron-right"></i>
                 </a>

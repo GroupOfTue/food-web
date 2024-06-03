435d6013b8c801b62e1ffd1827951d771af8ec98
Qthang <tqtbest@gmail.com>
2024-06-03 14:33:12 +0700

    fix button

--- src/pages/Aboutme/index.js
+++ src/pages/Aboutme/index.js
@@ -7,60 +7,91 @@ import images from '~/access/images';
 function Aboutme() {
   const cx = classNames.bind(styles);
   return (
-    <section className={(clsx('section-conten padding-y'), cx('container'))} style={{ 'minHeight': '84vh' }}>
-      <div className="card mx-auto" style={{ 'maxWidth': '1000px', 'marginTop': '10px' }}>
+    <section className={(clsx('section-conten padding-y'), cx('container'))} style={{ minHeight: '84vh' }}>
+      <div className="card mx-auto" style={{ maxWidth: '1000px', marginTop: '10px' }}>
         <div className="card-body">
           <h4 className="card-title mb-4">Account Information</h4>
           <table class="table">
             <thead>
               <tr>
-                <th scope="col"><svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                </svg> Profile Ficture</th>
-                <td scope="col"><button className='btn'>Upload</button></td>
+                <th scope="col">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="90"
+                    height="90"
+                    fill="currentColor"
+                    class="bi bi-person-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                    />
+                  </svg>{' '}
+                  Profile Ficture
+                </th>
+                <td scope="col">
+                  <button className={cx('btn')}>Upload</button>
+                </td>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <th scope="row"><p>Name</p>
+                <th scope="row">
+                  <p>Name</p>
                   <p class="label-Name">thangday</p>
                 </th>
-                <td><button className='btn'>Edit</button></td>
-
+                <td>
+                  <button className={cx('btn')}>Edit</button>
+                </td>
               </tr>
               <tr>
-                <th scope="row"><p>Email</p>
+                <th scope="row">
+                  <p>Email</p>
                   <p class="label-Email">thang@gmail.com</p>
                 </th>
-                <td><button className='btn'>Update</button></td>
-
+                <td>
+                  <button className={cx('btn')}>Update</button>
+                </td>
               </tr>
               <tr>
-                <th scope="row"><p>Mobile number</p>
+                <th scope="row">
+                  <p>Mobile number</p>
                   <p class="label-Mobilephone">+123 123 123</p>
                 </th>
-                <td><button className='btn'>Edit</button></td>
+                <td>
+                  <button className={cx('btn')}>Edit</button>
+                </td>
               </tr>
               <tr>
-                <th scope="row"><p>Date of birth</p>
+                <th scope="row">
+                  <p>Date of birth</p>
                   <p class="label-Date">-</p>
                 </th>
-                <td><button className='btn'>Edit</button></td>
+                <td>
+                  <button className={cx('btn')}>Edit</button>
+                </td>
               </tr>
               <tr>
-                <th scope="row"><p>Gender</p>
-                  <p class="label-Gender">-Chose your Gender-</p></th>
-                <td><button className='btn'>Edit</button></td>
+                <th scope="row">
+                  <p>Gender</p>
+                  <p class="label-Gender">-Chose your Gender-</p>
+                </th>
+                <td>
+                  <button className={cx('btn')}>Edit</button>
+                </td>
               </tr>
               <tr>
-                <th scope="row"><p>Password</p>
+                <th scope="row">
+                  <p>Password</p>
                   <p class="label-Password " type="password"></p>
                 </th>
 
-                <td><button className='btn'>Change Password</button></td>
+                <td>
+                  <button className={cx('btn')}>Change Password</button>
+                </td>
               </tr>
-
             </tbody>
           </table>
         </div>
